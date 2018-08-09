96eab865b0605209603b29b77e475ea9d9d8c37f
enriquehadoop <40671489+enriquehadoop@users.noreply.github.com>
2018-08-09 02:05:37 -0500

    Create p26.py
    Mongodb as Kafka Consumer

---
+++ p26.py
@@ -0,0 +1,26 @@
+from kafka import KafkaConsumer
+from pymongo import MongoClient
+import json
+
+
+
+try:
+   client = MongoClient('localhost',27017)
+   db = client.Twittersuse 
+   print("Connected successfully!!!")
+except:  
+   print("Could not connect to MongoDB")
+	
+consumer = KafkaConsumer('mexico') #mexico topic name
+for msg in consumer:
+    record = json.loads(msg.value)
+    username = record['username']
+    location = record['location']
+
+    try:
+       twitter_rec = {'name' :username,'location':location}
+       rec_id1 = db.terrorism.insert_one(twitter_rec)
+       print("Data inserted with record ids",rec_id1)
+    except:
+       print("Could not insertInMongo")
+
